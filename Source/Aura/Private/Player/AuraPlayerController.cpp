1c1e00659e06b2075c900511abb82c45bca79d98
RobotZward <fwy000501@outlook.com>
2025-05-22 16:14:11 +0800

    Set input mapping context for AuraPlayerController

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,4 +2,30 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	// check宏，用于当传入的变量非法时停止执行
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(
+		GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	// 鼠标配置
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
