a5dace844a4d34b5db4f54e356968f7642e2fc86
RobotZward <fwy000501@outlook.com>
2025-05-22 17:32:19 +0800

    Create AuraGameMode and camera.

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,16 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0, 400.f, 0);
+	GetCharacterMovement()->bConstrainToPlane = true;	// 将运动约束到平面
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;	// 开始时对齐到平面
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+}
---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright HigeNeko
+
+
+#include "Game/AuraGameModeBase.h"
+
