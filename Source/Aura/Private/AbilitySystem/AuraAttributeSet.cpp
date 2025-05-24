18a10819e97e7454487cc51d246621d80a1efe51
HigeNeko <fwy000501@outlook.com>
2025-05-24 11:26:24 +0800

    Assign Health Data.
    Create BP_OverlayWidgetController.
    Assign Health and MaxHealth to BP_OverlayWidgetController, which broadcast values of AuraAttributeSet.

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -6,7 +6,7 @@
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
-	InitHealth(10.f);
+	InitHealth(100.f);
 	InitMaxHealth(100.f);
 }
 
--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -3,3 +3,14 @@
 
 #include "UI/WidgetController/OverlayWidgetController.h"
 
+#include "AbilitySystem/AuraAttributeSet.h"
+
+void UOverlayWidgetController::BroadcastInitialValues()
+{
+	const UAuraAttributeSet* AuraAttributeSet = CastChecked<UAuraAttributeSet>(AttributeSet);
+
+	OnMaxHealthChanged.Broadcast(AuraAttributeSet->GetMaxHealth());
+	OnHealthChanged.Broadcast(AuraAttributeSet->GetHealth());
+
+	
+}
