abcb1ddce771660fe189013103fd62106c58a0ff
HigeNeko <fwy000501@outlook.com>
2025-05-29 13:57:04 +0800

    Create DataAsset to store AttributeInfo

---
+++ Source/Aura/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,24 @@
+﻿// Copyright HigeNeko
+
+
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+FAuraAttributeInfo UAttributeInfo::FindAttributeInfoFromTag(const FGameplayTag& AttributeTag, bool bLogNitFound)
+{
+	for (const FAuraAttributeInfo& Info : AttributeInformation)
+	{
+		if (Info.AttributeTag.MatchesTagExact(AttributeTag))
+		{
+			return Info;
+		}
+	}
+
+	if (bLogNitFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find info for AttributeTag [%s] on AttributeInfo [%s]."),
+		       *AttributeTag.ToString(),
+		       *GetNameSafe(this));
+	}
+
+	return FAuraAttributeInfo();
+}
