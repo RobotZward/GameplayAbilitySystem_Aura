540f829a399f2317f8c6ef2864e53b735328bb7f
HigeNeko <fwy000501@outlook.com>
2025-06-03 23:13:07 +0800

    Implementing block chance

--- Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -3,18 +3,25 @@
 
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
 
+#include "AuraGameplayTags.h"
 #include "AbilitySystem/AuraAttributeSet.h"
 
+// 使用经典C++定义方式，不需要反射
 struct AuraDamageStatics
 {
+	// 采用宏进行声明
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
 
 	AuraDamageStatics()
 	{
+		// 采用宏进行定义分别需要传入：UAttributeSet、AttributeName、CaptureSourceType、bSnapShot
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, BlockChance, Target, false);
 	}
 };
 
+// 采用静态函数和静态变量，每次获取的均为同一变量
 static const AuraDamageStatics& GetAuraDamageStatics()
 {
 	static AuraDamageStatics DStatics;
@@ -23,12 +30,15 @@ static const AuraDamageStatics& GetAuraDamageStatics()
 
 UExecCalc_Damage::UExecCalc_Damage()
 {
+	// 将Definition添加到捕获列表
 	RelevantAttributesToCapture.Add(GetAuraDamageStatics().ArmorDef);
+	RelevantAttributesToCapture.Add(GetAuraDamageStatics().BlockChanceDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
                                               FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
 {
+	// FGameplayEffectCustomExecutionParameters中可以获取到很多信息
 	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
 	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
 
@@ -37,19 +47,27 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 
 	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
 
+	// 构建FAggregatorEvaluateParameters
 	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
 	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
-	
+
 	FAggregatorEvaluateParameters EvaluationParameters;
 	EvaluationParameters.SourceTags = SourceTags;
 	EvaluationParameters.TargetTags = TargetTags;
-	
-	float Armor;
-	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetAuraDamageStatics().ArmorDef, EvaluationParameters,
-	                                                           Armor);
-	Armor = FMath::Max(0.f, Armor);
-	++Armor;
 
-	const FGameplayModifierEvaluatedData EvaluatedData(GetAuraDamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+	// Get Damage Set by Caller Magnitude
+	float Damage = Spec.GetSetByCallerMagnitude(FAuraGameplayTags::Get().Damage);
+
+	float TargetBlockChance = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetAuraDamageStatics().BlockChanceDef,
+	                                                           EvaluationParameters, TargetBlockChance);
+	TargetBlockChance = FMath::Max<float>(0.f, TargetBlockChance);
+
+	const bool bBlocked = FMath::RandRange(1, 100) < TargetBlockChance;
+	Damage = bBlocked ? Damage / 2.f : Damage;
+
+	// 可以对多个Attribute进行设置
+	const FGameplayModifierEvaluatedData EvaluatedData(UAuraAttributeSet::GetIncomingDamageAttribute(),
+	                                                   EGameplayModOp::Additive, Damage);
 	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
