b50f60635860829829ba7380c5646117ff9fea43
HigeNeko <fwy000501@outlook.com>
2025-06-05 23:17:24 +0800

    Create blank AIController, behavior tree, blackboard

---
+++ Source/Aura/Private/AI/AuraAIController.cpp
@@ -0,0 +1,31 @@
+﻿// Copyright HigeNeko
+
+
+#include "AI/AuraAIController.h"
+
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+AAuraAIController::AAuraAIController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Blackboard = CreateDefaultSubobject<UBlackboardComponent>("BlackboardComponent");
+	check(Blackboard);
+	BehaviorTreeComponent = CreateDefaultSubobject<UBehaviorTreeComponent>("BehaviorTreeComponent");
+	check(BehaviorTreeComponent);
+}
+
+void AAuraAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AAuraAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
---
+++ Source/Aura/Public/AI/AuraAIController.h
@@ -0,0 +1,27 @@
+﻿// Copyright HigeNeko
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Runtime/AIModule/Classes/AIController.h"
+#include "AuraAIController.generated.h"
+
+class UBlackboardComponent;
+class UBehaviorTreeComponent;
+
+UCLASS()
+class AURA_API AAuraAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	AAuraAIController();
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY()
+	TObjectPtr<UBehaviorTreeComponent> BehaviorTreeComponent;
+};
